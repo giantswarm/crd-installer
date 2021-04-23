add8f5797322a6e44489740b17a59c0a64ae36e7
Thomas Fussell <thomas@fussell.io>
2021-04-23 13:16:07 -0400

    use sh instead of bash

--- scripts/download-calico-crds.sh
+++ scripts/download-calico-crds.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
-set -euo pipefail
-IFS=$'\n\t'
+#!/bin/sh
+set -euo
 
 CALICO_VERSION=$1
+CALICO_VERSION_TRIMMED=$(echo "$CALICO_VERSION" | sed 's/v//') # trim leading v
 TMPDIR=$(mktemp -d)
 # for example, https://github.com/projectcalico/calico/tree/v3.16.0/_includes/charts/calico/crds/kdd
 wget https://github.com/projectcalico/calico/archive/refs/tags/"$CALICO_VERSION".zip -O "$TMPDIR"/calico.zip
-unzip -j "$TMPDIR"/calico.zip calico-"${CALICO_VERSION/v}"/_includes/charts/calico/crds/kdd/\*.yaml -d crds
+unzip -j "$TMPDIR"/calico.zip calico-"$CALICO_VERSION_TRIMMED"/_includes/charts/calico/crds/kdd/\*.yaml -d crds
