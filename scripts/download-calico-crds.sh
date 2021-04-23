549e6b6187ca1bc72661480b28d8d7fd7cda3b58
Thomas Fussell <thomas@fussell.io>
2021-04-23 13:06:44 -0400

    drop unzip u flag

--- scripts/download-calico-crds.sh
+++ scripts/download-calico-crds.sh
@@ -6,4 +6,4 @@ CALICO_VERSION=$1
 TMPDIR=$(mktemp -d)
 # for example, https://github.com/projectcalico/calico/tree/v3.16.0/_includes/charts/calico/crds/kdd
 wget https://github.com/projectcalico/calico/archive/refs/tags/"$CALICO_VERSION".zip -O "$TMPDIR"/calico.zip
-unzip -ju "$TMPDIR"/calico.zip calico-"${CALICO_VERSION/v}"/_includes/charts/calico/crds/kdd/\*.yaml -d crds
+unzip -j "$TMPDIR"/calico.zip calico-"${CALICO_VERSION/v}"/_includes/charts/calico/crds/kdd/\*.yaml -d crds
