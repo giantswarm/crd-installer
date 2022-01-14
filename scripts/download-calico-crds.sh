e427604fea4ebc47794b5fd9d247564b00a15076
Christian Bianchi <christian@giantswarm.io>
2022-01-14 10:55:36 +0100

    structure of the source code changed

--- scripts/download-calico-crds.sh
+++ scripts/download-calico-crds.sh
@@ -9,4 +9,4 @@ CALICO_VERSION_TRIMMED=$(echo "$CALICO_VERSION" | sed 's/v//') # trim leading v
 TMPDIR=$(mktemp -d)
 # Example final URL: https://github.com/projectcalico/calico/tree/v3.16.0/_includes/charts/calico/crds/kdd
 wget https://github.com/projectcalico/calico/archive/refs/tags/"$CALICO_VERSION".zip -O "$TMPDIR"/calico.zip
-unzip -j "$TMPDIR"/calico.zip calico-"$CALICO_VERSION_TRIMMED"/_includes/charts/calico/crds/kdd/\*.yaml -d crds
+unzip -j "$TMPDIR"/calico.zip calico-"$CALICO_VERSION_TRIMMED"/calico/_includes/charts/calico/crds/kdd/\*.yaml -d crds
